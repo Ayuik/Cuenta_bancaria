8639d8f17406c046690b11ed94b933ad5948d675
Ayuik <marcosayelen@gmail.com>
2025-03-25 16:54:10 +0100

    feat: added isActive attribute and getter. test passes

--- src/main/java/dev/ayelen/cuenta_bancaria/SavingsAccount.java
+++ src/main/java/dev/ayelen/cuenta_bancaria/SavingsAccount.java
@@ -1,9 +1,15 @@
 package dev.ayelen.cuenta_bancaria;
 
 public class SavingsAccount extends BankAccount {
+    boolean isActive;
 
     public SavingsAccount(float balance, float annualinterest) {
         super(balance, annualinterest);
+        this.isActive = true;
+    }
+
+    public boolean getIsActive(){
+        return isActive;
     }
 
 }
