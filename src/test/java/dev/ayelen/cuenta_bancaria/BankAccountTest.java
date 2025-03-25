74a5baa3793f323482dec84b88c476679605304d
Ayuik <marcosayelen@gmail.com>
2025-03-25 13:58:01 +0100

    test: setters fails

--- src/test/java/dev/ayelen/cuenta_bancaria/BankAccountTest.java
+++ src/test/java/dev/ayelen/cuenta_bancaria/BankAccountTest.java
@@ -2,6 +2,7 @@ package dev.ayelen.cuenta_bancaria;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,6 +23,25 @@ public class BankAccountTest {
         assertThat(monthlyfee, is(0F));
         assertThat(balance, is(1000F));
         assertThat(annualinterest, is (12F));  
+    }
+
+    @Test
+    @DisplayName("Should set new values for the attibutes")
+    void testSet(){
+        BankAccount myBankAccount = new BankAccount(1000F, 12F);
+        float updatedBalance = 8000F;
+        myBankAccount.setBalance(updatedBalance);
+        int deposits = 2;
+        myBankAccount.setDeposits(deposits);
+        float monthlyfee = 1000F;
+        myBankAccount.setMonthlyfee(monthlyfee);
+        int withdrawals = 5;
+        myBankAccount.setWithdrawals(withdrawals);
+
+        assertThat(myBankAccount.getBalance(), is(8000F));
+        assertThat(myBankAccount.getDeposits(), is (2));
+        assertThat(myBankAccount.getWithdrawals(), is (5));
+        assertThat(myBankAccount.getMonthlyfee(), is(1000F));
     }
     
 }
