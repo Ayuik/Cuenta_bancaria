357bac5be0562b42d9a78075d33ac2787974b98a
Ayuik <marcosayelen@gmail.com>
2025-03-25 17:09:29 +0100

    test: isActive_false fails

--- src/test/java/dev/ayelen/cuenta_bancaria/SavingsAccountTest.java
+++ src/test/java/dev/ayelen/cuenta_bancaria/SavingsAccountTest.java
@@ -1,5 +1,6 @@
 package dev.ayelen.cuenta_bancaria;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -11,21 +12,28 @@ public class SavingsAccountTest {
     SavingsAccount savingsAccount;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         savingsAccount = new SavingsAccount(10000F, 24F);
     }
 
     @Test
     @DisplayName("Should extend from BankAccount")
-    void testInheritance(){
+    void testInheritance() {
         assertTrue(savingsAccount instanceof BankAccount);
     }
 
     @Test
     @DisplayName("Should have an isActive attribute set to true")
-    void testIsActive(){
+    void testIsActive() {
         assertTrue(savingsAccount.getIsActive());
     }
-    
-    
+
+    @Test
+    @DisplayName("isActive should be false if balance is lower that 10000")
+    void testIsActive_false() {
+        savingsAccount.setBalance(5000);
+        savingsAccount.setIsActive();
+        assertFalse(savingsAccount.getIsActive());
+    }
+
 }
