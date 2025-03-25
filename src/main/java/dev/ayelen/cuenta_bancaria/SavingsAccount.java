d348e9d1109a4ba81d561b0555bd1db1d4227d23
Ayuik <marcosayelen@gmail.com>
2025-03-25 16:42:45 +0100

    feat: SavingsAccount class created, inheritance established. test passes

---
+++ src/main/java/dev/ayelen/cuenta_bancaria/SavingsAccount.java
@@ -0,0 +1,9 @@
+package dev.ayelen.cuenta_bancaria;
+
+public class SavingsAccount extends BankAccount {
+
+    public SavingsAccount(float balance, float annualinterest) {
+        super(balance, annualinterest);
+    }
+
+}
