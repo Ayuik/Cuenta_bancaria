8336570ec72565dd66411b32f76bbe74f3edb3f3
Ayuik <marcosayelen@gmail.com>
2025-03-25 16:56:56 +0100

    refactored: beforeeach implemented

--- src/test/java/dev/ayelen/cuenta_bancaria/SavingsAccountTest.java
+++ src/test/java/dev/ayelen/cuenta_bancaria/SavingsAccountTest.java
@@ -2,23 +2,30 @@ package dev.ayelen.cuenta_bancaria;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SavingsAccountTest {
 
+    SavingsAccount savingsAccount;
+
+    @BeforeEach
+    void setUp(){
+        savingsAccount = new SavingsAccount(10000F, 24F);
+    }
+
     @Test
     @DisplayName("Should extend from BankAccount")
     void testInheritance(){
-        SavingsAccount savingsAccount = new SavingsAccount(2000F, 24F);
         assertTrue(savingsAccount instanceof BankAccount);
     }
 
     @Test
     @DisplayName("Should have an isActive attribute set to true")
     void testIsActive(){
-        SavingsAccount savingsAccount = new SavingsAccount(10000F, 24F);
         assertTrue(savingsAccount.getIsActive());
     }
+    
     
 }
