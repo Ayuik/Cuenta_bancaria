6becd8f24cd16d94d4044a02ae52db4da96f0532
Ayuik <marcosayelen@gmail.com>
2025-03-27 11:24:42 +0100

    refactored: beforeeach implemented

--- src/test/java/dev/ayelen/cuenta_bancaria/CurrentAccountTest.java
+++ src/test/java/dev/ayelen/cuenta_bancaria/CurrentAccountTest.java
@@ -2,33 +2,36 @@ package dev.ayelen.cuenta_bancaria;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CurrentAccountTest {
+    CurrentAccount currentAccount;
+
+    @BeforeEach
+    void setUp() {
+        currentAccount = new CurrentAccount(1000F, 12F);
+    }
 
     @Test
     @DisplayName("Should extend from BankAccount")
     void testInheritance() {
-        CurrentAccount currentAccount = new CurrentAccount(1000F, 12F);
         assertTrue(currentAccount instanceof BankAccount);
     }
 
     @Test
     @DisplayName("Should have an overdraft attribute set to 0")
     void testOverdraft() {
-        CurrentAccount currentAccount = new CurrentAccount(1000F, 12F);
         assertTrue(currentAccount.getOverdraft() == 0F);
     }
 
     @Test
     @DisplayName("Should allow makeWithdrawal if money_withdrawal is higher than balance and set an overdraft")
     void makeWithdrawal() {
-        CurrentAccount currentAccount = new CurrentAccount(1000F, 12F);
         currentAccount.makeWithdrawal(1200F);
         assertTrue(currentAccount.getBalance() == 0F);
         assertTrue(currentAccount.getOverdraft() == 200F);
     }
-
     
 }
