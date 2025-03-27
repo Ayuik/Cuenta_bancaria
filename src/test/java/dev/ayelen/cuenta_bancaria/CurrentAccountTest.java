4d6d1c5693b8520c14df9087bb2f5eaab5607dd5
Ayuik <marcosayelen@gmail.com>
2025-03-27 10:49:57 +0100

    test: currentAccount inheritance fails

---
+++ src/test/java/dev/ayelen/cuenta_bancaria/CurrentAccountTest.java
@@ -0,0 +1,16 @@
+package dev.ayelen.cuenta_bancaria;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CurrentAccountTest {
+
+    @Test
+    @DisplayName("Should extend from BankAccount")
+    void testInheritance() {
+        CurrentAccount currentAccount = new CurrentAccount();
+        assertTrue(currentAccount instanceof BankAccount);
+    }
+}
