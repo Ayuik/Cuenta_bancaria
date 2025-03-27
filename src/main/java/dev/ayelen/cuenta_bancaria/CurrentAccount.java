c9d817884857794fd1c6e319ddc5cafc91d5844b
Ayuik <marcosayelen@gmail.com>
2025-03-27 10:54:21 +0100

    feat:  class created. inheritances established. test passes

---
+++ src/main/java/dev/ayelen/cuenta_bancaria/CurrentAccount.java
@@ -0,0 +1,9 @@
+package dev.ayelen.cuenta_bancaria;
+
+public class CurrentAccount extends BankAccount{
+
+    public CurrentAccount(float balance, float annualinterest) {
+        super(balance, annualinterest);
+    }
+    
+}
