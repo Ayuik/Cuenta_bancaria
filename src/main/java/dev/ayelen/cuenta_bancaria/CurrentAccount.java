b0ea2aac7ccf23867fdf356f1f13fed8982282a7
Ayuik <marcosayelen@gmail.com>
2025-03-27 11:20:54 +0100

    (back to makeWithdrawal) feat: makeWithdrawal implemented. test passes

--- src/main/java/dev/ayelen/cuenta_bancaria/CurrentAccount.java
+++ src/main/java/dev/ayelen/cuenta_bancaria/CurrentAccount.java
@@ -1,6 +1,6 @@
 package dev.ayelen.cuenta_bancaria;
 
-public class CurrentAccount extends BankAccount{
+public class CurrentAccount extends BankAccount {
     private float overdraft;
 
     public CurrentAccount(float balance, float annualinterest) {
@@ -12,5 +12,20 @@ public class CurrentAccount extends BankAccount{
         return overdraft;
     }
 
-    
+    public void setOverdraft(float overdraft) {
+        this.overdraft = overdraft;
+    }
+
+    public void makeWithdrawal(float withdrawal_money) {
+        if (withdrawal_money <= getBalance()) {
+            super.makeWithdrawal(withdrawal_money);
+        } else {
+            float overdraft = withdrawal_money - getBalance();
+            setOverdraft(overdraft);
+            setBalance(0F);
+            int updatedWithdrawals = getWithdrawals() + 1;
+            setWithdrawals(updatedWithdrawals);
+        }
+    }
+
 }
