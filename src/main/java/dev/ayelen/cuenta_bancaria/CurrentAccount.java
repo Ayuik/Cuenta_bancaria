c4c82cb89dc9c7d64d81e87ac2931e78913f3f1b
Ayuik <marcosayelen@gmail.com>
2025-03-27 11:14:23 +0100

    feat: created attribute. test passes

--- src/main/java/dev/ayelen/cuenta_bancaria/CurrentAccount.java
+++ src/main/java/dev/ayelen/cuenta_bancaria/CurrentAccount.java
@@ -1,11 +1,16 @@
 package dev.ayelen.cuenta_bancaria;
 
 public class CurrentAccount extends BankAccount{
+    private float overdraft;
 
     public CurrentAccount(float balance, float annualinterest) {
         super(balance, annualinterest);
+        this.overdraft = 0F;
     }
 
-    
+    public float getOverdraft() {
+        return overdraft;
+    }
+
     
 }
